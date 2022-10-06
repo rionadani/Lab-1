b870ef205592c34ed460b6bdebeeb6315c3fe4a9
rionadani <rionadani@gmail.com>
2022-10-06 14:37:45 +0700

    Mengerjakan Lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-     path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
 ]
